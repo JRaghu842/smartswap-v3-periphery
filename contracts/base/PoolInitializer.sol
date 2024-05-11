c3aabecbec05c4db2faaf61667e23fe4b1e2d5e6
JRaghu842 <jraghavendra842@gmail.com>
2024-05-11 11:46:55 +0530

    contract folder replaced and few path correction done

--- contracts/base/PoolInitializer.sol
+++ contracts/base/PoolInitializer.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity =0.7.6;
 
-import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol';
-import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
+import 'smartswap-v3-core/contracts/interfaces/ISmartSwapFactory.sol';
+import 'smartswap-v3-core/contracts/interfaces/ISmartSwapPool.sol';
 
 import './PeripheryImmutableState.sol';
 import '../interfaces/IPoolInitializer.sol';
 
-/// @title Creates and initializes V3 Pools
+/// @title Creates and initializes  Pools
 abstract contract PoolInitializer is IPoolInitializer, PeripheryImmutableState {
     /// @inheritdoc IPoolInitializer
     function createAndInitializePoolIfNecessary(
@@ -17,15 +17,15 @@ abstract contract PoolInitializer is IPoolInitializer, PeripheryImmutableState {
         uint160 sqrtPriceX96
     ) external payable override returns (address pool) {
         require(token0 < token1);
-        pool = IUniswapV3Factory(factory).getPool(token0, token1, fee);
+        pool = ISmartSwapFactory(factory).getPool(token0, token1, fee);
 
         if (pool == address(0)) {
-            pool = IUniswapV3Factory(factory).createPool(token0, token1, fee);
-            IUniswapV3Pool(pool).initialize(sqrtPriceX96);
+            pool = ISmartSwapFactory(factory).createPool(token0, token1, fee);
+            ISmartSwapPool(pool).initialize(sqrtPriceX96);
         } else {
-            (uint160 sqrtPriceX96Existing, , , , , , ) = IUniswapV3Pool(pool).slot0();
+            (uint160 sqrtPriceX96Existing, , , , , , ) = ISmartSwapPool(pool).slot0();
             if (sqrtPriceX96Existing == 0) {
-                IUniswapV3Pool(pool).initialize(sqrtPriceX96);
+                ISmartSwapPool(pool).initialize(sqrtPriceX96);
             }
         }
     }
